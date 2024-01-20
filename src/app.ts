55e245604b0473065d1a79222eead011946531cd
abharath <30704812+anshulBharath@users.noreply.github.com>
2024-01-20 17:05:50 -0600

    Testing github action.

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello world, this is Chef Mate!');
+  res.send('Hello world, this is Chef Mate! Test github action.');
 });
 
 app.listen(port, () => {
